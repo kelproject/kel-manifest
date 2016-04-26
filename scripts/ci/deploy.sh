9720af38d81708b926c45e963f61ea2842857d6f
Brian Rosner <brosner@eldarion.com>
2016-04-26 15:45:44 -0600

    Turn off caching for channels.json

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -6,6 +6,6 @@ here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 
 echo -n "Uploading $PROJECT $BUILD_TAG... "
 (
-    gsutil -q cp channels.json gs://release.kelproject.com/distro/channels.json
+    gsutil -q -h "Cache-Control:private, max-age=0, no-transform" cp channels.json gs://release.kelproject.com/distro/channels.json
     gsutil -q cp manifest.json gs://release.kelproject.com/distro/$BUILD_TAG/manifest.json
 ) && echo "done"
