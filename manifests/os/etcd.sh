633ab37044997d98cd9e950019edc70c21422f9a
Brian Rosner <brian@brosner.com>
2018-04-03 16:03:58 -0600

    etcd manifest tweaks

--- manifests/os/etcd.sh
+++ manifests/os/etcd.sh
@@ -15,14 +15,19 @@ ETCD_NODE_NAME="{{ etcd.get_node_name(i) }}"
 ETCD_NODE_DNS="{{ etcd.get_node_fqdn(i) }}"
 ETCD_INITIAL_NODES="{{ etcd.get_initial_nodes()|join(",") }}"
 ADVERTISE_IP=$(curl -s -H Metadata-Flavor:Google http://metadata.google.internal./computeMetadata/v1/instance/network-interfaces/0/ip)
+ETCD_DISK="/dev/disk/by-id/google-etcd-pd"
 
 # format persistence disk
 mkdir -p /mnt/etcd-pd
-/usr/share/oem/google-startup-scripts/safe_format_and_mount -m "mkfs.ext4 -F" /dev/disk/by-id/google-etcd-pd /mnt/etcd-pd
+mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard "${ETCD_DISK}"
+mount -o discard,defaults "${ETCD_DISK}" /mnt/etcd-pd
+chmod a+w /mnt/etcd-pd
+echo UUID=$(blkid -s UUID -o value "${ETCD_DISK}") /mnt/etcd-pd ext4 discard,defaults,nofail 0 2 | tee -a /etc/fstab
 
 mkdir -m 700 -p /mnt/etcd-pd/var/etcd/data.etcd
 chown -R etcd:etcd /mnt/etcd-pd/var/etcd
 
+mkdir -p /etc/systemd/system/etcd-member.service.d
 cat > /tmp/20-cl-etcd-member.conf <<EOF
 [Service]
 Environment="ETCD_DATA_DIR=/mnt/etcd-pd/var/etcd/data.etcd"
@@ -32,17 +37,9 @@ Environment="ETCD_OPTS=--name ${ETCD_NODE_NAME} \
   --advertise-client-urls http://${ETCD_NODE_DNS}:2379 \
   --listen-peer-urls http://0.0.0.0:2380 \
   --initial-advertise-peer-urls http://${ETCD_NODE_DNS}:2380 \
-  --initial-cluster s1=${ETCD_INITIAL_NODES} \
+  --initial-cluster ${ETCD_INITIAL_NODES} \
   --initial-cluster-token mytoken \
   --initial-cluster-state new \
-  --client-cert-auth \
-  --trusted-ca-file /etc/ssl/certs/etcd-root-ca.pem \
-  --cert-file /etc/ssl/certs/s1.pem \
-  --key-file /etc/ssl/certs/s1-key.pem \
-  --peer-client-cert-auth \
-  --peer-trusted-ca-file /etc/ssl/certs/etcd-root-ca.pem \
-  --peer-cert-file /etc/ssl/certs/s1.pem \
-  --peer-key-file /etc/ssl/certs/s1-key.pem \
   --auto-compaction-retention 1"
 EOF
 mv /tmp/20-cl-etcd-member.conf /etc/systemd/system/etcd-member.service.d/20-cl-etcd-member.conf
